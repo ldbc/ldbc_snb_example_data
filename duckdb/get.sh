127acd7fcf2a22760e12ac819e10f6a5eb69d9d6
Gabor Szarnyas <szarnyasg@gmail.com>
2021-01-22 00:25:07 +0100

    CI, script output

--- duckdb/get.sh
+++ duckdb/get.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-wget https://github.com/cwida/duckdb/releases/download/v0.2.3/duckdb_cli-linux-amd64.zip
+wget -q https://github.com/cwida/duckdb/releases/download/v0.2.3/duckdb_cli-linux-amd64.zip
 unzip duckdb_cli-linux-amd64.zip
