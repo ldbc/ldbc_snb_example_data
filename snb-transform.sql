1cd7e87dd773a7186c85849c6e54d8fe44deb02d
Gabor Szarnyas <szarnyasg@gmail.com>
2021-01-30 23:08:02 +0100

    Add transform script

--- snb-transform.sql
+++ snb-transform.sql
@@ -496,3 +496,40 @@ COPY (SELECT strftime(creationDate, '%Y-%m-%dT%H:%M:%S.%g+00:00') AS creationDat
 COPY (SELECT strftime(creationDate, '%Y-%m-%dT%H:%M:%S.%g+00:00') AS creationDate, Person1id, Person2id FROM Person_knows_Person)
   TO 'data/csv-singular-projected-fk/Person_knows_Person.csv'
   WITH (HEADER, DELIMITER '|');
+
+-- static-data-projected-fk-separate-labels
+COPY (SELECT id, name, url FROM Organisation WHERE type = 'Company')
+  TO 'data/static-data-projected-fk-separate-labels/Company.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, name, url FROM Organisation WHERE type = 'University')
+  TO 'data/static-data-projected-fk-separate-labels/University.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, isLocatedIn_Place FROM Organisation WHERE type = 'Company')
+  TO 'data/static-data-projected-fk-separate-labels/Company_isLocatedIn_Country.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, isLocatedIn_Place FROM Organisation WHERE type = 'University')
+  TO 'data/static-data-projected-fk-separate-labels/University_isLocatedIn_City.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, name, url FROM Place WHERE type = 'Continent')
+  TO 'data/static-data-projected-fk-separate-labels/Continent.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, name, url FROM Place WHERE type = 'Country')
+  TO 'data/static-data-projected-fk-separate-labels/Country.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, name, url FROM Place WHERE type = 'City')
+  TO 'data/static-data-projected-fk-separate-labels/City.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, isPartOf_Place FROM Place WHERE type = 'Country')
+  TO 'data/static-data-projected-fk-separate-labels/Country_isPartOf_Continent.csv'
+  WITH (HEADER, DELIMITER '|');
+
+COPY (SELECT id, isPartOf_Place FROM Place WHERE type = 'City')
+  TO 'data/static-data-projected-fk-separate-labels/City_isPartOf_Country.csv'
+  WITH (HEADER, DELIMITER '|');
