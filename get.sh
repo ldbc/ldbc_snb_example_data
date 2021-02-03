54661fa953384f6fc21a24ede4f5b5014c5cb370
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-04 00:30:50 +0100

    Bump DuckDB version

--- get.sh
+++ get.sh
@@ -3,5 +3,5 @@
 set -e
 set -o pipefail
 
-wget -q https://github.com/cwida/duckdb/releases/download/v0.2.3/duckdb_cli-linux-amd64.zip
+wget -q https://github.com/cwida/duckdb/releases/download/v0.2.4/duckdb_cli-linux-amd64.zip
 unzip duckdb_cli-linux-amd64.zip
