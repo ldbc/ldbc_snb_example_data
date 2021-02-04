10992cbee2352cf3dcec1e969072f97e755ca92f
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-04 14:53:33 +0100

    Add logging and configuration

--- proc.sh
+++ proc.sh
@@ -3,12 +3,27 @@
 set -e
 set -o pipefail
 
-export PATHVAR=`pwd`/data/raw
-export POSTFIX=_0_0.csv
+PATHVAR=${1:-"`pwd`/data/raw/"}
+
+if [ "$2" = "--no-header" ]; then
+    HEADER=
+else
+    HEADER=", HEADER"
+fi
+
+POSTFIX=_0_0.csv
 
 rm -rf ldbc.duckdb
+echo initialize schema
 cat schema/raw.sql | ./duckdb ldbc.duckdb
 cat schema/composite-merged-fk.sql | ./duckdb ldbc.duckdb
-cat snb-load.sql | sed "s|\${PATHVAR}|${PATHVAR}|g" | sed "s|\${POSTFIX}|${POSTFIX}|g" | ./duckdb ldbc.duckdb
+echo load data
+cat snb-load.sql | \
+  sed "s|\${PATHVAR}|${PATHVAR}|g" | \
+  sed "s|\${POSTFIX}|${POSTFIX}|g" | \
+  sed "s|\${HEADER}|${HEADER}|g" | \
+  ./duckdb ldbc.duckdb
+echo transform data
 cat snb-transform.sql | sed "s|:bulkLoadTime|'2014-01-01T00:00:00.000+00:00'|g" | ./duckdb ldbc.duckdb
+echo export data
 cat snb-export.sql | ./duckdb ldbc.duckdb
--- snb-load.sql
+++ snb-load.sql
@@ -1,26 +1,26 @@
 -- static entities
 
-COPY Raw_Organisation              FROM '${PATHVAR}/static/organisation${POSTFIX}'                 (DELIMITER '|', HEADER);
-COPY Raw_Place                     FROM '${PATHVAR}/static/place${POSTFIX}'                        (DELIMITER '|', HEADER);
-COPY Raw_Tag                       FROM '${PATHVAR}/static/tag${POSTFIX}'                          (DELIMITER '|', HEADER);
-COPY Raw_TagClass                  FROM '${PATHVAR}/static/tagclass${POSTFIX}'                     (DELIMITER '|', HEADER);
+COPY Raw_Organisation              FROM '${PATHVAR}/static/organisation${POSTFIX}'                 (DELIMITER '|' ${HEADER});
+COPY Raw_Place                     FROM '${PATHVAR}/static/place${POSTFIX}'                        (DELIMITER '|' ${HEADER});
+COPY Raw_Tag                       FROM '${PATHVAR}/static/tag${POSTFIX}'                          (DELIMITER '|' ${HEADER});
+COPY Raw_TagClass                  FROM '${PATHVAR}/static/tagclass${POSTFIX}'                     (DELIMITER '|' ${HEADER});
 
 -- dynamic entities
 
-COPY Raw_Comment                   FROM '${PATHVAR}/dynamic/comment${POSTFIX}'                     (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Comment_hasTag_Tag        FROM '${PATHVAR}/dynamic/comment_hasTag_tag${POSTFIX}'          (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Post                      FROM '${PATHVAR}/dynamic/post${POSTFIX}'                        (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Post_hasTag_Tag           FROM '${PATHVAR}/dynamic/post_hasTag_tag${POSTFIX}'             (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Forum                     FROM '${PATHVAR}/dynamic/forum${POSTFIX}'                       (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Forum_hasMember_Person    FROM '${PATHVAR}/dynamic/forum_hasMember_person${POSTFIX}'      (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Forum_hasTag_Tag          FROM '${PATHVAR}/dynamic/forum_hasTag_tag${POSTFIX}'            (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Person                    FROM '${PATHVAR}/dynamic/person${POSTFIX}'                      (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Person_hasInterest_Tag    FROM '${PATHVAR}/dynamic/person_hasInterest_tag${POSTFIX}'      (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Person_studyAt_University FROM '${PATHVAR}/dynamic/person_studyAt_organisation${POSTFIX}' (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Person_workAt_Company     FROM '${PATHVAR}/dynamic/person_workAt_organisation${POSTFIX}'  (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Person_likes_Post         FROM '${PATHVAR}/dynamic/person_likes_post${POSTFIX}'           (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Person_likes_Comment      FROM '${PATHVAR}/dynamic/person_likes_comment${POSTFIX}'        (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Comment                   FROM '${PATHVAR}/dynamic/comment${POSTFIX}'                     (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Comment_hasTag_Tag        FROM '${PATHVAR}/dynamic/comment_hasTag_tag${POSTFIX}'          (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Post                      FROM '${PATHVAR}/dynamic/post${POSTFIX}'                        (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Post_hasTag_Tag           FROM '${PATHVAR}/dynamic/post_hasTag_tag${POSTFIX}'             (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Forum                     FROM '${PATHVAR}/dynamic/forum${POSTFIX}'                       (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Forum_hasMember_Person    FROM '${PATHVAR}/dynamic/forum_hasMember_person${POSTFIX}'      (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Forum_hasTag_Tag          FROM '${PATHVAR}/dynamic/forum_hasTag_tag${POSTFIX}'            (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person                    FROM '${PATHVAR}/dynamic/person${POSTFIX}'                      (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person_hasInterest_Tag    FROM '${PATHVAR}/dynamic/person_hasInterest_tag${POSTFIX}'      (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person_studyAt_University FROM '${PATHVAR}/dynamic/person_studyAt_organisation${POSTFIX}' (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person_workAt_Company     FROM '${PATHVAR}/dynamic/person_workAt_organisation${POSTFIX}'  (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person_likes_Post         FROM '${PATHVAR}/dynamic/person_likes_post${POSTFIX}'           (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person_likes_Comment      FROM '${PATHVAR}/dynamic/person_likes_comment${POSTFIX}'        (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
 
 -- Person-KNOWS-Person edges are undirected so they should exist in both ways
-COPY Raw_Person_knows_Person ( creationDate, deletionDate, explicitlyDeleted, person1id, person2id) FROM '${PATHVAR}/dynamic/person_knows_person${POSTFIX}' (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
-COPY Raw_Person_knows_Person ( creationDate, deletionDate, explicitlyDeleted, person2id, person1id) FROM '${PATHVAR}/dynamic/person_knows_person${POSTFIX}' (DELIMITER '|', HEADER, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person_knows_Person ( creationDate, deletionDate, explicitlyDeleted, person1id, person2id) FROM '${PATHVAR}/dynamic/person_knows_person${POSTFIX}' (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
+COPY Raw_Person_knows_Person ( creationDate, deletionDate, explicitlyDeleted, person2id, person1id) FROM '${PATHVAR}/dynamic/person_knows_person${POSTFIX}' (DELIMITER '|' ${HEADER}, TIMESTAMPFORMAT '%Y-%m-%dT%H:%M:%S.%g+00:00');
