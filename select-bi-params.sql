21328fbe7e3fc0b18e4e3880679ce7513bfb88e5
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-15 00:32:38 +0100

    Some params

--- select-bi-params.sql
+++ select-bi-params.sql
@@ -1,2 +1,10 @@
 SELECT * FROM Message_TagClasses LIMIT 10 OFFSET 10;
 SELECT * FROM Message_creationDays LIMIT 10 OFFSET 10;
+SELECT * FROM Country_numCities LIMIT 2;
+SELECT * FROM Country_numPersons LIMIT 2;
+SELECT * FROM Message_Tags LIMIT 10 OFFSET 10;
+
+-- Q14a
+SELECT * FROM CountryPairs_numFriends ORDER BY frequency DESC LIMIT 10;
+-- Q14b
+SELECT * FROM CountryPairs_numFriends ORDER BY frequency ASC  LIMIT 10;
