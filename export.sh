245a4a74e626124327cdbb4872e452659447599f
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-14 13:46:52 +0100

    Move export scripts to dir

--- export.sh
+++ export.sh
@@ -3,8 +3,8 @@
 set -e
 set -o pipefail
 
-echo export data
-for export_script in snb-export-*.sql; do
+echo "export data"
+for export_script in export/*.sql; do
   echo ${export_script}
   cat ${export_script} | ./duckdb ldbc.duckdb
 done
