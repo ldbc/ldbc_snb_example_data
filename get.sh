0cb8b4a7068961f18eca2469a7fb2c9f4d7db7d4
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-14 13:48:07 +0100

    Make wget quieter

--- get.sh
+++ get.sh
@@ -6,7 +6,7 @@ set -o pipefail
 export DUCKDB_VERSION=v0.2.5
 
 if [ ! -f duckdb ]; then
-    wget https://github.com/cwida/duckdb/releases/download/${DUCKDB_VERSION}/duckdb_cli-linux-amd64.zip -O duckdb_cli-linux-amd64.zip
+    wget -q https://github.com/cwida/duckdb/releases/download/${DUCKDB_VERSION}/duckdb_cli-linux-amd64.zip -O duckdb_cli-linux-amd64.zip
     unzip -o duckdb_cli-linux-amd64.zip
     rm duckdb_cli-linux-amd64.zip
 fi
