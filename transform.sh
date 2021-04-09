47179ee1d12466c3e68a1be8f0a0808a2c0d5f16
Gabor Szarnyas <szarnyasg@gmail.com>
2021-04-09 19:56:24 +0200

    Remove unused batching variable from script

--- transform.sh
+++ transform.sh
@@ -4,4 +4,5 @@ set -e
 set -o pipefail
 
 echo "transform data from raw to composite-merged-fk format"
-cat snb-transform.sql | sed "s|:bulkLoadTime|DATE '2013-01-01T00:00:00.000+00:00'|g" | ./duckdb ldbc.duckdb
+#cat snb-transform.sql | sed "s|:bulkLoadTime|DATE '2013-01-01T00:00:00.000+00:00'|g" | 
+cat snb-transform.sql | ./duckdb ldbc.duckdb
