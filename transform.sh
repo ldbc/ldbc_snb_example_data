3c92f4e76af0ef83bbc7118c4fcd82e4a945d26d
Gabor Szarnyas <szarnyasg@gmail.com>
2021-04-09 22:20:01 +0200

    Add export script

--- transform.sh
+++ transform.sh
@@ -6,3 +6,4 @@ set -o pipefail
 echo "transform data from raw to composite-merged-fk format"
 #cat snb-transform.sql | sed "s|:bulkLoadTime|DATE '2013-01-01T00:00:00.000+00:00'|g" | 
 cat snb-transform.sql | ./duckdb ldbc.duckdb
+cat export/snb-export-singular-merged-fk.sql | ./duckdb ldbc.duckdb
