70448764eb27ffb7d579685190bdea1af6986d4c
Gabor Szarnyas <szarnyasg@gmail.com>
2021-05-04 16:15:21 +0200

    Fix SQL script path

--- generate-batches.sh
+++ generate-batches.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 DUCKDB_PATH="${DUCKDB_PATH:=.}"
 
-cat schema/deletes.sql | ${DUCKDB_PATH}/duckdb ldbc.duckdb
+cat workflow-sql/sql/initialize-delete-candidate-tables.sql | ${DUCKDB_PATH}/duckdb ldbc.duckdb
 python3 batches.py
