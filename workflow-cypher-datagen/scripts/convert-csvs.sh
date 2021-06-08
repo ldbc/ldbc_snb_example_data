bbdf8a6c35165b7d73aa54bc36652d749bb7c0dc
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-08 15:10:14 +0200

    Drop unused env var (the postfix is now the unambiguous '.csv')

--- workflow-cypher-datagen/scripts/convert-csvs.sh
+++ workflow-cypher-datagen/scripts/convert-csvs.sh
@@ -8,7 +8,6 @@ echo "Starting preprocessing CSV files"
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 : ${NEO4J_CSV_DIR:?"Environment variable NEO4J_CSV_DIR is unset or empty"}
-: ${NEO4J_CSV_POSTFIX:?"Environment variable NEO4J_CSV_POSTFIX is unset or empty"}
 
 # provide progressbar is available
 if command -v pv > /dev/null 2>&1; then
@@ -25,11 +24,11 @@ while read line; do
 
   echo ${FILENAME}: ${HEADER}
   # replace header (no point using sed to save space as it creates a temporary file as well)
-  if [ ! -f ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX} ]; then
-    echo "${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX} does not exist"
+  if [ ! -f ${NEO4J_CSV_DIR}/${FILENAME}.csv ]; then
+    echo "${NEO4J_CSV_DIR}/${FILENAME}.csv does not exist"
     exit 1
   fi
-  echo ${HEADER} | ${SNB_CAT} - <(tail -n +2 ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}) > tmpfile.csv && mv tmpfile.csv ${NEO4J_CSV_DIR}/${FILENAME}${NEO4J_CSV_POSTFIX}
+  echo ${HEADER} | ${SNB_CAT} - <(tail -n +2 ${NEO4J_CSV_DIR}/${FILENAME}.csv) > tmpfile.csv && mv tmpfile.csv ${NEO4J_CSV_DIR}/${FILENAME}.csv
 done < headers.txt
 
 echo "Finished preprocessing CSV files"
--- workflow-cypher-datagen/scripts/environment-variables-default.sh
+++ workflow-cypher-datagen/scripts/environment-variables-default.sh
@@ -7,5 +7,4 @@ export NEO4J_HEADER_DIR=`pwd`/headers
 export NEO4J_VERSION=4.2.5
 export NEO4J_ENV_VARS=""
 export NEO4J_CSV_DIR=`pwd`/../data/csv-composite-projected-fk
-export NEO4J_CSV_POSTFIX=.csv
 export NEO4J_CONTAINER_NAME="neo-ldbc-data-test"
