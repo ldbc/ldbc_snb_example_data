82de62b1ce25cf77e890c48e840e92d6be6be9c6
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-08 15:10:47 +0200

    Add pushd/popd to env vars script

--- workflow-cypher-datagen/scripts/environment-variables-default.sh
+++ workflow-cypher-datagen/scripts/environment-variables-default.sh
@@ -1,3 +1,5 @@
+pushd .
+
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
@@ -8,3 +10,5 @@ export NEO4J_VERSION=4.2.5
 export NEO4J_ENV_VARS=""
 export NEO4J_CSV_DIR=`pwd`/../data/csv-composite-projected-fk
 export NEO4J_CONTAINER_NAME="neo-ldbc-data-test"
+
+popd
