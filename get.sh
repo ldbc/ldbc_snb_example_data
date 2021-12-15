54b2aeb226d9f94e23e7db309cc623843f6dc0b3
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-15 10:45:52 +0100

    Bump DuckDB version

--- get.sh
+++ get.sh
@@ -3,11 +3,11 @@
 set -eu
 set -o pipefail
 
-DUCKDB_VERSION=v0.2.7
+DUCKDB_VERSION=v0.3.1
 DUCKDB_PATH="${DUCKDB_PATH:=.}"
 
 if [ ! -f ${DUCKDB_PATH}/duckdb ]; then
-    wget -q https://github.com/cwida/duckdb/releases/download/${DUCKDB_VERSION}/duckdb_cli-linux-amd64.zip -O duckdb_cli-linux-amd64.zip
+    wget -q https://github.com/duckdb/duckdb/releases/download/${DUCKDB_VERSION}/duckdb_cli-linux-amd64.zip -O duckdb_cli-linux-amd64.zip
     unzip -o duckdb_cli-linux-amd64.zip
     rm duckdb_cli-linux-amd64.zip
     pip3 install --user --progress-bar off duckdb==${DUCKDB_VERSION}
